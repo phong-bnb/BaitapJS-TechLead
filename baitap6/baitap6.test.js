4dea7dbaa2c0f6a23ef4fe86c12ab9feebca5c91
Phong-BNB <ngphong.0708@gmail.com>
2023-09-25 23:14:29 +0700

    update- test

--- baitap6/baitap6.test.js
+++ baitap6/baitap6.test.js
@@ -5,3 +5,27 @@ test("hello bay be [1,2,3,4,5,6]", ()=>{
     console.log(logger)
     expect(logger).toEqual([1,2,3,4,5,6]);
 });
+test("hello bay be [1,2,3,10,56,1000]", ()=>{
+    const arr = [10,1000,56,3,2,1];
+    const logger = SelectionSort(arr)
+    console.log(logger)
+    expect(logger).toEqual([1,2,3,10,56,1000]);
+});
+test("hello bay be [1,2,3,4.5,5,6]", ()=>{
+    const arr = [6,5,4.5,3,2,1];
+    const logger = SelectionSort(arr)
+    console.log(logger)
+    expect(logger).toEqual([1,2,3,4.5,5,6]);
+});
+test("hello bay be []", ()=>{
+    const arr = [];
+    const logger = SelectionSort(arr)
+    console.log(logger)
+    expect(logger).toEqual([]);
+});
+test("hello bay be [1,1,2,2,2,3,4,5,6,6,8]", ()=>{
+    const arr = [6,5,4,3,2,1,1,2,2,6,8];
+    const logger = SelectionSort(arr)
+    console.log(logger)
+    expect(logger).toEqual([1,1,2,2,2,3,4,5,6,6,8]);
+});
